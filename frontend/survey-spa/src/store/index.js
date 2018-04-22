a1bcca76448fcf495c0fa402f05c423aa21e0be4
progayk <drum.ayk@gmail.com>
2018-04-22 13:38:25 +0300

    add vuex, create store, and register vuex to main.js

---
+++ frontend/survey-spa/src/store/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// The state object will serve as the single source of truth where all the important application-level
+// data is contained within the store. This state object will contain survey data that can be accessed
+// and watched for changes by any components interested in them such as the Home component
+const state = {
+  // single source of data
+}
+
+// The actions object is where I will define what are known as action methods.
+//  Action methods are referred to as being "dispatched" and they're used to
+// handle asynchronous operations such as AJAX calls to an external service or API.
+const actions = {
+  // asynchronous operations
+}
+
+// The mutations object provides methods which are referred to being "committed" and serve as the one
+// and only way to change the state of the data in the state object. When a mutation is committed any
+// components that are referencing the now reactive data in the state object are updated with the new
+// values, causing the UI to update and re-render its elements.
+const mutations = {
+  // isolated data mutations
+}
+
+// The getters object contains methods also, but in this case they serve to access the state data
+// utilizing some logic to return information. Getters are useful for reducing code
+// duplication and promote reusability across many components.
+const getters = {
+  // reusable data accessors
+}
+
+const store = new Vuex.Store({
+  state,
+  actions,
+  mutations,
+  getters
+})
+
+export default store
